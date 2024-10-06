c6718bad3085caa24e39cc08a60077c9337d4855
aub3men <aub3men@posteitaliane.it>
2024-10-06 16:53:53 +0200

    primo commit

---
+++ src/main/java/com/lista/spesa/ListaSpesa/ListaSpesaApplication.java
@@ -0,0 +1,13 @@
+package com.lista.spesa.ListaSpesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaSpesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaSpesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lista/spesa/ListaSpesa/ListaSpesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lista.spesa.ListaSpesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaSpesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
